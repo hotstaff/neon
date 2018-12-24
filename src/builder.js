f6ae61e7fc66c1df2a699480553366c8dd7c5c3a
Hideto Manjo <enjinit@gmail.com>
2018-12-24 18:27:46 +0900

    [release] Experimental release.

--- src/builder.js
+++ src/builder.js
@@ -1,55 +1,79 @@
 /*
  NEON - html builder
-    Wrote by Hideto Manjo
+    Wrote by Hideto Manjo 2018
 */
 
-/*jslint node: true */
+/*jslint node: false */
+/*jslint stupid: false */
 
 "use strict";
+// time variable
+var start_time;
+
+/* Define */
+var SITE_JSON_NAME;
+var SITE_JSON;
+var SOURCE_DIR;
+var DEST_DIR;
+
+/* Instance */
+var watcher;
+
+/* Import */
+var chokidar = require("chokidar");
 var fs = require("fs");
 var path = require("path");
 
 var MD = require('markdown-it')({
     typographer: true,
+    linkify: true,
 });
+
 MD.use(require('markdown-it-title'))
-  .use(require('markdown-it-emoji'))
-  .use(require('markdown-it-ins'))
-  .use(require('markdown-it-mark'))
-  .use(require('markdown-it-footnote'))
-  .use(require('markdown-it-deflist'))
-  .use(require('markdown-it-abbr'))
-  .use(require('markdown-it-sub'))
-  .use(require('markdown-it-sup'))
-  .use(require('markdown-it-container'))
-  .use(require('markdown-it-highlightjs'));
+    .use(require('markdown-it-emoji'))
+    .use(require('markdown-it-ins'))
+    .use(require('markdown-it-mark'))
+    .use(require('markdown-it-footnote'))
+    .use(require('markdown-it-deflist'))
+    .use(require('markdown-it-abbr'))
+    .use(require('markdown-it-sub'))
+    .use(require('markdown-it-sup'))
+    .use(require('markdown-it-container'))
+    .use(require('markdown-it-highlightjs'))
+    .use(require('markdown-it-imsize'));
 
 
-// define
-if (process.argv.length < 3) {
-    console.log("Please input site.json.")
-    process.exit(1);
-}
-var SITE_JSON_NAME = process.argv[2];
-var LOAD_SITE_JSON = false;
 
-// common function
+console.log(`
+
+oooo   oooo ooooooooooo  ooooooo  oooo   oooo 
+ 8888o  88   888    88 o888   888o 8888o  88  
+ 88 888o88   888ooo8   888     888 88 888o88  
+ 88   8888   888    oo 888o   o888 88   8888  
+o88o    88  o888ooo8888  88ooo88  o88o    88 
+                                             
+`);
+
+
+// Common function
+// Defined at parsing time.
 function isExistFile(file) {
     try {
         fs.statSync(file);
-        return true
-    } catch(err) {
-        if(err.code === 'ENOENT') return false
+        return true;
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            return false;
+        }
+        return null;
     }
 }
 
 function isExistDir(dirname) {
-
     if (fs.existsSync(dirname) && fs.statSync(dirname).isDirectory() === false) {
         return false;
     }
-    console.log(dirname)
-    fs.access(dirname, fs.constants.R_OK | fs.constants.W_OK, function(error){
+    fs.access(dirname, fs.constants.R_OK | fs.constants.W_OK, function (error) {
         if (error) {
             if (error.code === "ENOENT") {
                 fs.mkdirSync(dirname);
@@ -59,33 +83,50 @@ function isExistDir(dirname) {
     return true;
 }
 
+/* Common function end */
+
+/* Define */
+if (process.argv.length < 3) {
+    console.log("Please input site.json.");
+    process.exit(1);
+}
+
+SITE_JSON_NAME = path.resolve(process.argv[2]);
+SITE_JSON = {};
+
 // Read SITE.json
-if (SITE_JSON_NAME !== undefined && isExistFile(SITE_JSON_NAME)){
-   var SITE_JSON = JSON.parse(fs.readFileSync(SITE_JSON_NAME, 'utf8'));
-   LOAD_SITE_JSON = true;
-}else{
-    console.log("Not Found (site.json) input filename ->" + SITE_JSON_NAME );
+if (isExistFile(SITE_JSON_NAME) === false) {
+    if (isExistDir(SITE_JSON_NAME) === false) {
+        console.log("Not Found (site.json) input filename ->" + SITE_JSON_NAME);
+        process.exit(1);
+    }
+    SITE_JSON_NAME = path.resolve(SITE_JSON_NAME, "./site.json");
+}
+
+try{
+    SITE_JSON = JSON.parse(fs.readFileSync(SITE_JSON_NAME, 'utf8'));    
+} catch(e) {
+    console.log("JSON Parse Error site.json");
+    console.log(e);
     process.exit(1);
 }
 
 // common configuration
-var SITE_JSON = SITE_JSON || {};
-var SOURCE_DIR = path.dirname(path.resolve(LOAD_SITE_JSON ? SITE_JSON_NAME : ".")) + "/";
-var DEST_DIR =  path.resolve(SOURCE_DIR + SITE_JSON.dest || ".") + "/";
+SOURCE_DIR = path.dirname(SITE_JSON_NAME);
+DEST_DIR =  path.resolve(SOURCE_DIR, SITE_JSON.dest || "./") ;
 
 // dest dir check
-if (isExistDir(DEST_DIR)) {
-    console.log("Source directory: " + SOURCE_DIR);
-    console.log("Dest directory: " + DEST_DIR);
-} else {
-    console.log("Dest name file is already exists." + DEST_DIR);
+if (isExistDir(DEST_DIR) === false) {
+    console.log("Dest filename already exists. (" + DEST_DIR +")");
     process.exit(1);
 }
+
+console.log("Source directory: " + SOURCE_DIR);
+console.log("Dest directory: " + DEST_DIR);
 
 // START UP CHECK END
 
 // TEMPLETE START
-
 /* html templete builder 
    these code shuld more smart.*/
 var construct_index_html = function construct_index_html(index_json) {
@@ -141,10 +182,10 @@ ${md_html}
 
 var convert2html = function convert2html(file) {
     return new Promise(function(onFulfilled, onRejected) {
-        fs.readFile(SOURCE_DIR + file, "utf8", function(err, md_text){
+        fs.readFile(path.resolve(SOURCE_DIR, file), "utf8", function(err, md_text){
             if (err) onRejected("Convert failure(read): " + file);
             var ret = {};
-            fs.writeFile(DEST_DIR + file.replace(/.md/, ".htm"), construct_page_html(MD.render(md_text, ret)), function(err){
+            fs.writeFile(path.resolve(DEST_DIR, file.replace(/.md/, ".htm")), construct_page_html(MD.render(md_text, ret)), function(err){
                 if (err) onRejected("Convert failure(write): " + file);
                 ret.source = file;
                 onFulfilled(ret);
@@ -160,7 +201,8 @@ var obtain_markdown_files = function obtain_markdown_files (dirname) {
                 onRejected("Do not obtain markdown: " + dirname);
             }
             onFulfilled(files.filter(function(file){
-                return fs.statSync(dirname + file).isFile() && /.*\.md$/.test(dirname + file); //絞り込み
+                var file_path = path.resolve(dirname, file);
+                return fs.statSync(file_path).isFile() && /.*\.md$/.test(file_path); //絞り込み
             }));
         });
     });
@@ -190,12 +232,12 @@ var convert_all = function convert_all() {
             }));
         }).then(
             function(md_files){
-                console.log("Converted!!");
+                console.log("HTML generation succeeded!");
                 onFulfilled(md_files);
             },
             function(err){
                 console.log(err.message);
-                onRejected("Error.");
+                onRejected("HTML generation Error.");
             })
     });
 }
@@ -210,12 +252,44 @@ var write_html = function write_html(fname, html_text) {
 }
 
 /* main sequence */
-Promise.resolve()
+var build_all = function build_all(){
+    start_time = Date.now();
+    Promise.resolve()
     .then(convert_all)
     .then(construct_index_json)
     .then(function(index_json){
         console.log(index_json);
-        write_html(DEST_DIR + "menu.htm", construct_menu_html(index_json));
-        write_html(DEST_DIR + "index.htm", construct_index_html(index_json));
+        write_html(path.resolve(DEST_DIR, "menu.htm"), construct_menu_html(index_json));
+        write_html(path.resolve(DEST_DIR, "index.htm"), construct_index_html(index_json));
         return index_json;
     })
+    .then(function(){
+        console.log("Time: " + Number(Date.now() - start_time) + " msec.");
+    })
+
+};
+
+build_all();
+
+watcher = chokidar.watch(SOURCE_DIR,{
+    ignored:/[\/\\]\./,
+    persistent:true
+});
+
+watcher.on('ready',function(){
+
+    watcher.on('add',function(file_path){
+        if (path.extname(file_path) === ".md") {
+            console.log(file_path + " added.");
+            build_all();  
+        }
+    });
+
+    watcher.on('change',function(file_path){
+        if (path.extname(file_path) === ".md") {
+            console.log(file_path + " changed.");
+            build_all();    
+        }
+    });
+});
+
